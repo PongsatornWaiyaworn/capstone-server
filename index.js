c2100674dabb6cba468589a53ad0bb8b4ee6a765
PongsatornWaiyaworn <117834631+PongsatornWaiyaworn@users.noreply.github.com>
2025-05-18 19:04:09 +0700

    edit

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ app.use(express.json());
 require('dotenv').config(); 
 app.use(cors({
     origin: true,
+    credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE','PATCH'], 
     allowedHeaders: ['Content-Type', 'Authorization']
   }));
